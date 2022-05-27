f72a5643cfaa196fedc33e633ad9357d2607b9c3
MasumBhai <abdullahmasum6035@gmail.com>
2022-05-28 02:35:41 +0600

    upto loops done...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,21 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'Learning-Angular';
+  displayVal: string = ''
+  truth_value = false;
+  show = false;
+  val_01 = "Masum"
+  val_02 = "Hello"
+  color01 = "Blue"
+  array_01 = ['Abdullah', 'Al', 'Masum', 'Bhai', 'Jindabaad']
+  userDetails = [
+    {name: 'Masum', email: 'abdullahmasum6035@gmail.com', phone: '01551805248', accounts:['yahoo','facebook','linkedIn']},
+    {name: 'Ishrak Latif', email: 'aztechvelly@gmail.com', phone: '01615001206',accounts:['gmail','telegram','Instagram']},
+    {name: 'Guy', email: 'helloWorld035@gmail.com', phone: '01817001206',accounts:['outlook','what\'s app','linkedIn']},
+    {name: 'Don\'t Know', email: 'ultapaltapagla@gmail.com', phone: '01681143600',accounts:['snapchat','facebook','linkedIn']},
+    {name: 'Brainy Fool', email: 'brainy_fools@gmail.com', phone: '01533942468',accounts:['gmail','facebook','twitter']},
+  ];
+
 
   first_fun() {
     return "Function has been called";
@@ -15,7 +30,13 @@ export class AppComponent {
   click_event_fn01(name: string | number) { /* parameter:any or change in tsconfig.json's strict:false*/
     alert(name + " looks suspicious..!")
   }
-  click_event_fn02(val:string){
-  console.warn(val)
+
+  click_event_fn02(val: string) {
+    console.warn(val)
+  }
+
+  getting_inputValue01(val: any) {
+    console.warn(val)
+    this.displayVal = val;
   }
 }
